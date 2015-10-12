2a92df636b3a144be7c93ef42d860077ff2a3bb2
misasagi <kosmos@misasagideMacBook-Air.local>
2015-10-12 08:20:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = function(dir){
+  var libs = {};
+  function readdir(dir,father){
+    function isEmpty(obj){
+	    for(var i in obj){
+		    return false;
+	    }
+	    return true;
+    }
+    if(!/\/$/.test(dir)){
+      dir += "/";
+    }
+	  if(!father){
+		  father = [];
+	  }
+	  var point = libs;
+	  for(var i = 0;i < father.length;i ++){
+		  point = point[father[i]];
+	  }
+	  var lib = fs.readdirSync(dir);
+	  lib.forEach(function(v){
+		  var st = fs.statSync(dir + v);
+		  if(st.isDirectory()){
+			  var f = father.slice();
+			  f.push(v);
+			  if(!point[v]){
+				  point[v] = {};
+			  }
+			  readdir(dir+v,f);
+		  }else if(st.isFile()){
+			  if(/.js$/.test(v)){
+				  var name = v.split('.js')[0];
+				  var req =  require(dir+v);
+				  if(isEmpty(point[name])){
+					  point[name] = req;
+				  }else{
+					  for(var i in req){
+						  point[name][i] = req[i];
+					  }
+				  }
+			  }
+		  }
+	  });
+  }
+  readdir(dir);
+  return libs;
+};
+
