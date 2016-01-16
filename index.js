05563bd938e498d562e2875e79e5852c54818e2f
misasagi <kosmos@misasagideMacBook-Air.local>
2016-01-16 19:05:23 +0800

    0.0.5

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
 var fs = require('fs');
 
-module.exports = function(dir,projectDir){
-  if(!/\/$/.test(projectDir)){
-    projectDir += "/";
-  }
-  dir = projectDir + dir;
-  var libs = {};
-  function readdir(dir,father){
-    function isEmpty(obj){
-	    for(var i in obj){
-		    return false;
-	    }
-	    return true;
+function readDir(dir){
+    "use strict";
+    var all = {};
+    var files = fs.readdirSync(dir);
+    for(var i = 0;i < files.length;i ++){
+        let filePath = dir + "/" + files[i];
+        if(fs.statSync(filePath).isDirectory()){
+            all[files[i]] = readDir(filePath);
+        }else{
+            if(/.js$/.test(filePath)){
+                all[files[i].split(".js")[0]] = require(filePath);
+            }
+        }
     }
-    if(!/\/$/.test(dir)){
-      dir += "/";
-    }
-	  if(!father){
-		  father = [];
-	  }
-	  var point = libs;
-	  for(var i = 0;i < father.length;i ++){
-		  point = point[father[i]];
-	  }
-	  var lib = fs.readdirSync(dir);
-	  lib.forEach(function(v){
-		  var st = fs.statSync(dir + v);
-		  if(st.isDirectory()){
-			  var f = father.slice();
-			  f.push(v);
-			  if(!point[v]){
-				  point[v] = {};
-			  }
-			  readdir(dir+v,f);
-		  }else if(st.isFile()){
-			  if(/.js$/.test(v)){
-				  var name = v.split('.js')[0];
-				  var req = require(dir+v);
-				  if(isEmpty(point[name])){
-					  point[name] = req;
-				  }else{
-					  for(var i in req){
-						  point[name][i] = req[i];
-					  }
-				  }
-			  }
-		  }
-	  });
-  }
-  readdir(dir);
-  return libs;
-};
+    return all;
+}
+
+module.exports = readDir;
 
